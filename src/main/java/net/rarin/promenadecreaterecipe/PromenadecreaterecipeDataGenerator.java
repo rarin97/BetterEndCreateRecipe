90716cdb7f956f6e35d847f0c7ceace88859aa5e
rarin97 <minecraftboyzach@hotmail.com>
2025-07-13 12:46:49 +1000

    Initial commit

---
+++ src/main/java/net/rarin/promenadecreaterecipe/PromenadecreaterecipeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rarin.promenadecreaterecipe;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PromenadecreaterecipeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/rarin/promenadecreaterecipe/Promenadecreaterecipeclient.java
@@ -0,0 +1,10 @@
+package net.rarin.promenadecreaterecipe;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Promenadecreaterecipeclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
